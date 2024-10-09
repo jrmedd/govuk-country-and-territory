245002758cd3d5c3ede52384cb0ea841b8089a82
James Medd <james.medd@sparck.io>
2024-10-09 17:24:07 +0100

    Support a default value

--- scripts/load.js
+++ scripts/load.js
@@ -2,7 +2,7 @@ window.addEventListener('load', event => {
   document.querySelectorAll('[data-module="govuk-country-and-territory-autocomplete"]').forEach(element => {
     openregisterLocationPicker({
       selectElement: document.querySelector('[data-module="govuk-country-and-territory-autocomplete"]'),
-      defaultValue: '',
+      defaultValue: element.value,
       name: element.name,
       url: '/plugin-assets/govuk-country-and-territory/scripts/location-autocomplete-graph.json'
     })
